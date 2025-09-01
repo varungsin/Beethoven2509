15ef6b856be6850aeb308b5acb75b8736632eaf0
Varun V (varuv) <varuv@cisco.com>
2025-09-01 10:55:59 +0530

    msg

---
+++ 01-09-2025/find_sum.py
@@ -0,0 +1,13 @@
+#  find sum
+''' 
+find_sum
+args : firstNum , SecondNum
+returns sum of numbers
+'''
+
+def find_sum(firstNum,SecondNum):
+    result = firstNum + SecondNum
+    return result 
+
+print(find_sum(10,20))
+print(find_sum(1,2))
