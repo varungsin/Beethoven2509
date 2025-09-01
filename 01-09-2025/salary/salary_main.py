e006efc71d62f3d6f6040e145636d9a8d46767b2
Varungsin <varungsin01@gmail.com>
2025-09-01 16:31:08 +0530

    updated code

--- 01-09-2025/salary/salary_main.py
+++ 01-09-2025/salary/salary_main.py
@@ -44,22 +44,6 @@ def menus():
     choice = menu()
     while choice != 6:
         choice = menu()
-create_salary(1000)
-create_salary(2000)
-create_salary(3000)
-create_salary(4000) 
-create_salary(8000) 
+    print(' Thank you for using App')
 
-result_salaries= read_all()
-for salary in result_salaries:
-    print(salary)
-    
-print(read_by_salary(3000)) #1
-print(read_by_salary(8000)) #3
-print(salaries[read_by_salary(5000)]) #-1
-
-update(8000,8500)
-print(read_all())
-
-delete_by_salary(2000)
-print(read_all())
+menus()
